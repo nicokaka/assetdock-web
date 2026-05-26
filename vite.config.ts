3336fa8bfbfcf0c23a13098d2beb2a12b2229651
Nicolas <sansung700@gmail.com>
2026-05-26 09:55:59 -0300

    chore: proxy Actuator health endpoints to avoid CORS

--- vite.config.ts
+++ vite.config.ts
@@ -21,6 +21,10 @@ export default defineConfig({
         target: 'http://localhost:8080',
         changeOrigin: true,
       },
+      '/actuator': {
+        target: 'http://localhost:8081',
+        changeOrigin: true,
+      },
     },
   },
 })
